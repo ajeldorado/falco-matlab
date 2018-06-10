4568d63375c8fb24973f59ea13aa097b3ec93d8c
A J Eldorado Riggs <A.J.Eldorado.Riggs@jpl.nasa.gov>
2018-06-10 16:23:20 -0500

    Bug fix in tip/tilt offsets
    Before was accidentally including twice the 1/lambda shift in lambda/D
    of a tip/tilt offset.

--- models/model_Jacobian_SPLC.m
+++ models/model_Jacobian_SPLC.m
@@ -50,8 +50,8 @@ NdmPad = DM.compact.NdmPad;
 %--Include the tip/tilt in the input wavefront
 if(isfield(mp,'ttx'))
     %--Scale by lambda/lambda0 because ttx and tty are in lambda0/D
-    x_offset = mp.ttx(modvar.ttIndex)*(mp.lambda0/lambda);
-    y_offset = mp.tty(modvar.ttIndex)*(mp.lambda0/lambda);
+    x_offset = mp.ttx(modvar.ttIndex);
+    y_offset = mp.tty(modvar.ttIndex);
 
     TTphase = (-1)*(2*pi*(x_offset*mp.P2.compact.XsDL + y_offset*mp.P2.compact.YsDL));
     Ett = exp(1i*TTphase*mp.lambda0/lambda);
--- models/model_full_LC.m
+++ models/model_full_LC.m
@@ -64,8 +64,8 @@ else % Default to using the starlight
     %--Include the tip/tilt in the input stellar wavefront
     if(isfield(mp,'ttx'))
         %--Scale by lambda/lambda0 because ttx and tty are in lambda0/D
-        x_offset = mp.ttx(modvar.ttIndex)*(mp.lambda0/lambda);
-        y_offset = mp.tty(modvar.ttIndex)*(mp.lambda0/lambda);
+        x_offset = mp.ttx(modvar.ttIndex);
+        y_offset = mp.tty(modvar.ttIndex);
 
         TTphase = (-1)*(2*pi*(x_offset*mp.P2.full.XsDL + y_offset*mp.P2.full.YsDL));
         Ett = exp(1i*TTphase*mp.lambda0/lambda);
