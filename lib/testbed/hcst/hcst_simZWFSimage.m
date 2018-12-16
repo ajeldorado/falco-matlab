47380e9f1ebb5b771294e2153a470bd107b8d9ca
Garreth Ruane <ruane.gary@gmail.com>
2018-12-15 20:34:46 -0800

    Functions for ZWFS reconstruct
    Including new functions for ZWFS reconstruction.

--- lib/testbed/hcst/hcst_simZWFSimage.m
+++ lib/testbed/hcst/hcst_simZWFSimage.m
@@ -2,30 +2,30 @@
 function Isum = hcst_simZWFSimage(mp)
 
 
-%--Compute the DM surfaces outside the full model to save lots of time
-if(any(mp.dm_ind==1)); mp.dm1.surfM = falco_gen_dm_surf(mp.dm1,mp.dm1.dx,mp.dm1.NdmPad); end
+    %--Compute the DM surfaces outside the full model to save lots of time
+    if(any(mp.dm_ind==1)); mp.dm1.surfM = falco_gen_dm_surf(mp.dm1,mp.dm1.dx,mp.dm1.NdmPad); end
 
-Isum = 0; % Initialize image
+    Isum = 0; % Initialize image
 
-for si=1:mp.Nsbp
-    modvar.sbpIndex = si;
-    
-    for wi=1:mp.Nwpsbp
-        modvar.whichSource = 'star';
-        modvar.wpsbpIndex = wi;
-        Etemp = model_hcst_ZWFS(mp, modvar);
-        
-        Isum = Isum + (abs(Etemp).^2)*mp.sbp_weights(si)*mp.full.lambda_weights(wi);
-    end 
-    
-    if(mp.planetFlag)
-        modvar.whichSource = 'exoplanet';
-        Eout = model_hcst_ZWFS(mp,modvar);
-        ImPlanetC = abs(Eout).^2; % In contrast
-        Isum = Isum + ImPlanetC*mp.sbp_weights(si);
+    for si=1:mp.Nsbp
+        modvar.sbpIndex = si;
+
+        for wi=1:mp.Nwpsbp
+            modvar.whichSource = 'star';
+            modvar.wpsbpIndex = wi;
+            Etemp = model_hcst_ZWFS(mp, modvar);
+
+            Isum = Isum + (abs(Etemp).^2)*mp.sbp_weights(si)*mp.full.lambda_weights(wi);
+        end 
+
+        if(mp.planetFlag)
+            modvar.whichSource = 'exoplanet';
+            Eout = model_hcst_ZWFS(mp,modvar);
+            ImPlanetC = abs(Eout).^2; % In contrast
+            Isum = Isum + ImPlanetC*mp.sbp_weights(si);
+        end
+
     end
-
-end
 
 end %--END OF FUNCTION
 
