90d6579d989faadf1289b693c0600f81c1ea5381
Garreth Ruane <ruane.gary@gmail.com>
2019-03-23 14:40:24 -0700

    Generalized testbed interface
    Modified the testbed interface to select which  falco_get_*_sbp_image function to call depending on the mp.testbed string. e.g. mp.testbed = 'hcst' calls the function to get the image from the hcst when mp.flatSim = false.

--- lib/imaging/falco_get_testbed_sbp_image.m
+++ lib/imaging/falco_get_testbed_sbp_image.m
@@ -4,10 +4,8 @@
 % at the California Institute of Technology.
 % -------------------------------------------------------------------------
 %
-% Function to get an image in the specified sub-bandpass from the Caltech
-% HCST testbed. This function will need to be replaced in order to run on a
-% different testbed. Note that the number of pixels per lambda*F# is
-% predetermined. 
+% Function to get an image in the specified sub-bandpass from a testbed. 
+% This function calls an equivalent sub-function depending on mp.testbed. 
 %
 % ---------------
 % INPUTS:
@@ -20,39 +18,22 @@
 %           by a photometry measurement at a single offset)
 %
 % REVISION HISTORY
-% - Created on 2019-02-22 by G. Ruane 
+% - Created on 2019-03-22 by G. Ruane 
 
 
 function normI = falco_get_testbed_sbp_image(mp,si)
     
-    %----- Send commands to the DM -----
-    disp('Sending current DM voltages to testbed') 
-    
-    map = mp.dm1.V'; % There's a transpose between Matlab and BMC indexing
-    
-    % Send the commands to the DM. 
-    % Notes: bench.DM.flatvec contains the commands to flatten the DM. 
-    %        mp.dm1.V is added to the flat commands inside
-    %        hcst_DM_apply2Dmap. 
-    %        FALCO allows for searches through multiplicative gains which
-    %        are handled outside of this function. 
-    cmds = hcst_DM_apply2Dmap(bench,map,1);% Returns actual DM commands 
-    
-    %----- Get image from the testbed -----
-    disp(['Getting image from testbed in band',num2str(si)])
-    
-    % disp('Setting varia to bandpass',num2str(si)])
-    %   NOT IMPLEMENTED YET!!! ONLY WILL WORK FOR MONOCHROMATIC EFC
-    
-    % Load the dark with the correct tint. It must exist in the dark
-    % library. 
-    dark = hcst_andor_loadDark(bench,[bench.info.path2darks,'dark_tint',num2str(bench.andor.tint,2),'_coadds1.fits']);
-    
-    % Scale the PSF photometry by the current integration time
-    peakPSF = mp.peakPSF/mp.peakPSFtint*bench.andor.tint; 
-    
-    % Get normalized intenstiy (dark subtracted and normalized by peakPSF)
-    normI = (hcst_andor_getImage(bench)-dark)/peakPSF; 
+    switch upper(mp.testbed)
+        case 'HCST'
+            normI = falco_get_hcst_sbp_image(mp,si);
+        case 'GPCT'
+            normI = falco_get_gpct_sbp_image(mp,si); 
+        %Can put other testbeds or fancy models here
+        %case 'WHATEVER'
+            %normI = falco_get_whatever_sbp_image(mp,si); 
+        otherwise
+            normI = falco_get_sim_sbp_image(mp,si);   
+    end
     
 end 
 
