4cc519fd8d8cdede0a1a0db3461e6c6bae3f321e
A J Eldorado Riggs <aj.riggs@jpl.nasa.gov>
2019-06-24 10:54:16 -0700

    Added Keck pupil option.

--- lib/Zernike/falco_get_Zernike_sensitivities.m
+++ lib/Zernike/falco_get_Zernike_sensitivities.m
@@ -36,7 +36,6 @@ for ni = 1:Nannuli
     [maskCube(:,:,ni), ~, ~] = falco_gen_SW_mask(maskStruct);
 end
 
-
 if(mp.full.flagPROPER==false)  %--When using built-in FALCO full models
     %%--Generate Zernike map datacube
     ZmapCube = falco_gen_norm_zernike_maps(mp.P1.full.Nbeam,mp.centering,indsZnoll); %--Cube of normalized (RMS = 1) Zernike modes.
@@ -90,7 +89,6 @@ inds_list_zern = allcomb(1:mp.full.NlamUnique,1:Npol,1:Nzern).'; %--dimensions: 
 NvalsZern = size(inds_list_zern,2);
 
 %--Get nominal, unaberrated final E-field at each wavelength and polarization
-%EZarray = zeros(mp.Fend.Neta,mp.Fend.Nxi,mp.full.NlamUnique,Npol,Nzern); %--initialize
 dEZarray = zeros(mp.Fend.Neta,mp.Fend.Nxi,mp.full.NlamUnique,Npol,Nzern); %--initialize 
 
 %--Obtain all the images in parallel
@@ -108,86 +106,8 @@ fprintf('done. Time = %.2f s\n',toc);
         ipol = inds_list_zern(2,ni);
         izern = inds_list_zern(3,ni);
         dEZarray(:,:,ilam,ipol,izern) = Estruct{ni}  - E0array(:,:,ilam,ipol); %--Compute the delta E-field
-%         EZarray(:,:,ilam,ipol,izern) = Estruct{ni}; 
-%         dEZarray(:,:,ilam,ipol,izern) = EZarray(:,:,ilam,ipol,izern) - E0array(:,:,ilam,ipol); %--Compute the delta E-field
     end
     clear Estruct
-
-% %     %--Get aberrated, final focal-plane E-fields for each Zernike mode
-% %     EZarray = zeros(mp.Fend.Neta,mp.Fend.Nxi,Nzern,Nvals); %--initialize
-% %     dEZarray = zeros(size(EZarray));
-%     
-%     if(isfield(mp.full,'zindex')==false);  mp.full.zindex = [];  mp.full.zval_m = [];  end %--Initialize the Zernike modes to include as empty if the variable doesn't exist already
-%     zindex0 = mp.full.zindex; %--Save the original
-%     zval_m0 = mp.full.zval_m; %--Save the original
-%     for iz = 1:Nzern
-%         for ni = 1:mp.Nsbp  
-%             mp.full.zindex = zindex0; %--Reset
-%             mp.full.zval_m = zval_m0; %--Reset
-%                        
-%             %--Put the Zernike index and coefficent in the vectors used by the PROPER full model
-%             if(any(zindex0==indsZnoll(iz))) %--Add the delta to an existing entry
-%                 zind = find(zindex0==indsZnoll(iz));
-%                 mp.full.zval_m(zind) = mp.full.zval_m(zind) + 1e-9;
-%             else %--Concatenate the Zenike modes to the vector if it isn't included already
-%                 mp.full.zindex = [mp.full.zindex(:),indsZnoll(iz)];
-%                 mp.full.zval_m = [zval_m0(:), 1e-9]; % [meters]
-%             end
-%             
-%             %--Call the full model
-%             ilam = inds_list(1,ni);
-%             ipol = inds_list(2,ni);
-%             mp.full.polaxis = mp.full.pol_conds(ipol);
-%             si = mp.full.indsLambdaMat(ilam,1);
-%             wi = mp.full.indsLambdaMat(ilam,2);
-%             modvar.sbpIndex = si;
-%             modvar.wpsbpIndex = wi;
-%             modvar.whichSource = 'star';
-%             EZarray(:,:,iz,ni) = model_full(mp, modvar);
-%             dEZarray(:,:,iz,ni) = EZarray(:,:,iz,ni)-E0cube(:,:,ni); %--Compute the delta E-field
-%         end
-%     end
-    
-    
-% % else %--Use a full model included in FALCO
-% % --NOT UPDATED YET
-%     %%--Generate Zernike map datacube
-%     ZmapCube = falco_gen_norm_zernike_maps(mp.P1.compact.Nbeam,mp.centering,indsZnoll); %--Cube of normalized (RMS = 1) Zernike modes.
-%     %--Make sure ZmapCube is the right array size
-%     if(size(ZmapCube,1)~=mp.P1.compact.Narr)
-%         ZmapCubeTemp = zeros(mp.P1.compact.Narr,mp.P1.compact.Narr);
-%         for zi=1:size(ZmapCube,3)
-%             ZmapCubeTemp(:,:,zi) = padOrCropEven(ZmapCube(:,:,zi),mp.P1.compact.Narr);
-%         end
-%         ZmapCube = ZmapCubeTemp; 
-%         clear ZmapCubeTemp
-%     end
-% 
-%     %%--Get nominal, unaberrated final E-field at each wavelength.
-%     mp.P1.compact.E = ones(mp.P1.compact.Narr,mp.P1.compact.Narr,mp.Nsbp); %--Input E-fields
-%     E0cube = zeros(mp.Fend.Neta,mp.Fend.Nxi,mp.Nsbp); %--initialize
-%     for si = 1:mp.Nsbp   
-%         modvar.sbpIndex = si;
-%         modvar.whichSource = 'star';     
-%         E0cube(:,:,si) = model_compact(mp, modvar);
-%     end
-% 
-%     %%--Get aberrated, final focal-plane E-fields for each Zernike mode
-%     EZarray = zeros(mp.Fend.Neta,mp.Fend.Nxi,Nzern,mp.Nsbp); %--initialize
-%     dEZarray = zeros(size(EZarray));
-%     for iz = 1:Nzern
-%         for si = 1:mp.Nsbp      
-%             mp.P1.compact.E(:,:,si) = exp(1i*2*pi/mp.sbp_centers(si)*mp.full.ZrmsVal*ZmapCube(:,:,iz));%.*mp.P1.compact.E(:,:,si);       
-%             modvar.sbpIndex = si;
-%             modvar.whichSource = 'star';     
-%             EZarray(:,:,iz,si) = model_compact(mp, modvar);
-%             dEZarray(:,:,iz,si) = EZarray(:,:,iz,si)-E0cube(:,:,si);
-%         end
-%     end
-%     
-    
-% end
-
 
 %% Compute Zernike sensitivity values averaged across each annulus (or annular sector) in the dark hole
 
