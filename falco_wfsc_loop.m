6273ba114c115eb4a68fff434708a8e7d31e41a0
A J Eldorado Riggs <aj.riggs@jpl.nasa.gov>
2021-01-20 15:19:54 -0600

    More storage arrays. More cleanup.
    - Added storage arrays out.IrawCorrHist and out.IrawScoreHist.
    - Put more code from falco_wfsc_loop.m into smaller functions.
    - More linting in falco_wfsc_loop.

--- falco_wfsc_loop.m
+++ falco_wfsc_loop.m
@@ -13,10 +13,13 @@ Im = falco_get_summed_image(mp);
 
 % Begin the Estimation+Control Iterations
 for Itr = 1:mp.Nitr
+    
+    ev.Itr = Itr;
+    cvar.Itr = Itr;
 
     %% Apply DM constraints now. Can't do within DM surface generator if calling a PROPER model. 
-    if(any(mp.dm_ind==1));  mp.dm1 = falco_enforce_dm_constraints(mp.dm1);  end
-    if(any(mp.dm_ind==2));  mp.dm2 = falco_enforce_dm_constraints(mp.dm2);  end
+    if any(mp.dm_ind == 1);  mp.dm1 = falco_enforce_dm_constraints(mp.dm1);  end
+    if any(mp.dm_ind == 2);  mp.dm2 = falco_enforce_dm_constraints(mp.dm2);  end
     
     %%
     %--Start of new estimation+control iteration
@@ -35,19 +38,19 @@ for Itr = 1:mp.Nitr
     fprintf('DMs to be used in this iteration = ['); for jj = 1:length(mp.dm_ind); fprintf(' %d',mp.dm_ind(jj)); end; fprintf(' ]\n');
 
     %--Fill in History of DM commands to Store
-    if(isfield(mp,'dm1')); if(isfield(mp.dm1,'V'));  out.dm1.Vall(:,:,Itr) = mp.dm1.V;  end;  end
-    if(isfield(mp,'dm2')); if(isfield(mp.dm2,'V'));  out.dm2.Vall(:,:,Itr) = mp.dm2.V;  end;  end
-    if(isfield(mp,'dm8')); if(isfield(mp.dm8,'V'));  out.dm8.Vall(:,Itr) = mp.dm8.V(:);  end;  end
-    if(isfield(mp,'dm9')); if(isfield(mp.dm9,'V'));  out.dm9.Vall(:,Itr) = mp.dm9.V(:);  end;  end
+    if isfield(mp, 'dm1'); if(isfield(mp.dm1,'V'));  out.dm1.Vall(:,:,Itr) = mp.dm1.V;  end;  end
+    if isfield(mp, 'dm2'); if(isfield(mp.dm2,'V'));  out.dm2.Vall(:,:,Itr) = mp.dm2.V;  end;  end
+    if isfield(mp, 'dm8'); if(isfield(mp.dm8,'V'));  out.dm8.Vall(:,Itr) = mp.dm8.V(:);  end;  end
+    if isfield(mp, 'dm9'); if(isfield(mp.dm9,'V'));  out.dm9.Vall(:,Itr) = mp.dm9.V(:);  end;  end
 
     %--Compute the DM surfaces
-    if(any(mp.dm_ind==1)); DM1surf = falco_gen_dm_surf(mp.dm1, mp.dm1.compact.dx, mp.dm1.compact.Ndm); else; DM1surf = zeros(mp.dm1.compact.Ndm); end
-    if(any(mp.dm_ind==2)); DM2surf = falco_gen_dm_surf(mp.dm2, mp.dm2.compact.dx, mp.dm2.compact.Ndm); else; DM2surf = zeros(mp.dm2.compact.Ndm); end
+    if any(mp.dm_ind == 1); DM1surf = falco_gen_dm_surf(mp.dm1, mp.dm1.compact.dx, mp.dm1.compact.Ndm); else; DM1surf = zeros(mp.dm1.compact.Ndm); end
+    if any(mp.dm_ind == 2); DM2surf = falco_gen_dm_surf(mp.dm2, mp.dm2.compact.dx, mp.dm2.compact.Ndm); else; DM2surf = zeros(mp.dm2.compact.Ndm); end
 
     %% Throughput and Normalized Intensity
     %--Calculate the core throughput (at higher resolution to be more accurate)
     [mp, thput, ImSimOffaxis] = falco_compute_thput(mp);
-    if(mp.flagFiber)
+    if mp.flagFiber
         mp.thput_vec(Itr) = max(thput);
     else
         mp.thput_vec(Itr) = thput;
@@ -55,70 +58,36 @@ for Itr = 1:mp.Nitr
     
     %--Compute the current normalized intensity level
     out.InormHist(Itr) = mean(Im(mp.Fend.corr.maskBool));
+    out.IrawCorrHist(Itr) = mean(Im(mp.Fend.corr.maskBool));
+    out.IrawScoreHist(Itr) = mean(Im(mp.Fend.score.maskBool));
     
     %% Updated selection of Zernike modes targeted by the controller
     %--Decide with Zernike modes to include in the Jacobian
-    if(Itr==1); mp.jac.zerns0 = mp.jac.zerns; end
+    if Itr == 1; mp.jac.zerns0 = mp.jac.zerns; end
     fprintf('Zernike modes used in this Jacobian:\t'); fprintf('%d ',mp.jac.zerns); fprintf('\n');
     
     %--Re-compute the Jacobian weights
     mp = falco_set_jacobian_weights(mp); 
 
-    %% Actuator Culling: Initialization of Flag and Which Actuators
-
-    %--If new actuators are added, perform a new cull of actuators.
-    if(Itr==1)
-        cvar.flagCullAct = true;
-    else
-        if(isfield(mp,'dm_ind_sched'))
-            cvar.flagCullAct = ~isequal(mp.dm_ind_sched{Itr}, mp.dm_ind_sched{Itr-1});
-        else
-            cvar.flagCullAct = false;
-        end
-    end
-    mp.flagCullActHist(Itr) = cvar.flagCullAct;
-
-    %--Before performing new cull, include all actuators again
-    if(cvar.flagCullAct)
-        %--Re-include all actuators in the basis set. Need act_ele to be a column vector.
-        if(any(mp.dm_ind==1)); mp.dm1.act_ele = (1:mp.dm1.NactTotal).'; end
-        if(any(mp.dm_ind==2)); mp.dm2.act_ele = (1:mp.dm2.NactTotal).'; end
-        if(any(mp.dm_ind==8)); mp.dm8.act_ele = (1:mp.dm8.NactTotal).'; end
-        if(any(mp.dm_ind==9)); mp.dm9.act_ele = (1:mp.dm9.NactTotal).'; end
-        %--Update the number of elements used per DM
-        if(any(mp.dm_ind==1)); mp.dm1.Nele = length(mp.dm1.act_ele); else; mp.dm1.Nele = 0; end
-        if(any(mp.dm_ind==2)); mp.dm2.Nele = length(mp.dm2.act_ele); else; mp.dm2.Nele = 0; end
-        if(any(mp.dm_ind==8)); mp.dm8.Nele = length(mp.dm8.act_ele); else; mp.dm8.Nele = 0; end
-        if(any(mp.dm_ind==9)); mp.dm9.Nele = length(mp.dm9.act_ele); else; mp.dm9.Nele = 0; end
-    end
-
     %% Compute the control Jacobians for each DM
-    
-    %--Relinearize about the DMs only at the iteration numbers in mp.relinItrVec.
-    if any(mp.relinItrVec == Itr)
-        cvar.flagRelin = true;
-    else
-        cvar.flagRelin = false;
-    end
-    
-    if (Itr == 1) || cvar.flagRelin
+    if (Itr == 1) || any(mp.relinItrVec == Itr)
         jacStruct =  model_Jacobian(mp); %--Get structure containing Jacobians
     end
-
-    %% Cull weak actuators, but only if(cvar.flagCullAct && cvar.flagRelin)
-    [mp, jacStruct] = falco_ctrl_cull(mp, cvar, jacStruct);
+    
+    %% Cull weak actuators from the Jacobian
+    [mp, jacStruct] = falco_ctrl_cull_weak_actuators(mp, cvar, jacStruct);
 
     %% Load the improved Jacobian if using the E-M technique
     if mp.flagUseLearnedJac
         jacStructLearned = load('jacStructLearned.mat');
-        if(any(mp.dm_ind==1));  jacStruct.G1 = jacStructLearned.G1;  end
-        if(any(mp.dm_ind==1));  jacStruct.G2 = jacStructLearned.G2;  end
+        if any(mp.dm_ind == 1);  jacStruct.G1 = jacStructLearned.G1;  end
+        if any(mp.dm_ind == 1);  jacStruct.G2 = jacStructLearned.G2;  end
     end
 
     %% Wavefront Estimation
     
     %--Previous iteration's E-field for computing and plotting Delta E
-    if(Itr > 1)
+    if Itr > 1
         EestPrev = Eest;
         EsimPrev = Esim;
     end
@@ -132,9 +101,7 @@ for Itr = 1:mp.Nitr
         Esim(:, si) = Etemp(mp.Fend.corr.maskBool);
     end
     clear modvar Etemp
-    
-    ev.Itr = Itr;
-    
+        
     ev = falco_est(mp, ev, jacStruct);
     
     Eest = ev.Eest;
@@ -188,47 +155,28 @@ for Itr = 1:mp.Nitr
     end
     
     %% Add spatially-dependent (and star-dependent) weighting to the control Jacobians
-
-    for iStar = 1:mp.compact.star.count
-        if(any(mp.dm_ind==1))
-            jacStruct.G1(:, :, mp.jac.star_inds == iStar) = ...
-                jacStruct.G1(:, :, mp.jac.star_inds == iStar) .* repmat(mp.WspatialVec(:, iStar), [1, mp.dm1.Nele, mp.jac.NmodePerStar]);
-        end
-        if(any(mp.dm_ind==2))
-            jacStruct.G2(:, :, mp.jac.star_inds == iStar) = ...
-                jacStruct.G2(:, :, mp.jac.star_inds == iStar) .* repmat(mp.WspatialVec(:, iStar), [1, mp.dm2.Nele, mp.jac.NmodePerStar]);
-        end
-        if(any(mp.dm_ind==8))
-            jacStruct.G8(:, :, mp.jac.star_inds == iStar) = ...
-                jacStruct.G8(:, :, mp.jac.star_inds == iStar) .* repmat(mp.WspatialVec(:, iStar), [1, mp.dm8.Nele, mp.jac.NmodePerStar]);
-        end
-        if(any(mp.dm_ind==9))
-            jacStruct.G9(:, :, mp.jac.star_inds == iStar) = ...
-                jacStruct.G9(:, :, mp.jac.star_inds == iStar) .* repmat(mp.WspatialVec(:, iStar), [1, mp.dm9.Nele, mp.jac.NmodePerStar]);
-        end
-    end
-
-    %--Compute the number of total actuators for all DMs used. 
-    cvar.NeleAll = mp.dm1.Nele + mp.dm2.Nele + mp.dm3.Nele + mp.dm4.Nele + mp.dm5.Nele + mp.dm6.Nele + mp.dm7.Nele + mp.dm8.Nele + mp.dm9.Nele; %--Number of total actuators used 
+    jacStruct = falco_apply_spatial_weighting_to_Jacobian(mp, jacStruct);
     
     %% Wavefront Control
     
     cvar.Eest = Eest;
-    cvar.Itr = Itr;
-    cvar.out.InormHist = out.InormHist(Itr);
+    cvar.NeleAll = mp.dm1.Nele + mp.dm2.Nele + mp.dm3.Nele + mp.dm4.Nele + mp.dm5.Nele + mp.dm6.Nele + mp.dm7.Nele + mp.dm8.Nele + mp.dm9.Nele; %--Number of total actuators used 
     [mp, cvar] = falco_ctrl(mp, cvar, jacStruct);
     if isfield(cvar, 'Im') && ~mp.ctrl.flagUseModel
-        Im = cvar.Im; 
+        Im = cvar.Im;
+        out.InormHist(Itr+1) = mean(Im(mp.Fend.corr.maskBool));
+        out.IrawCorrHist(Itr+1) = mean(Im(mp.Fend.corr.maskBool));
+        out.IrawScoreHist(Itr+1) = mean(Im(mp.Fend.score.maskBool));
     end
     
     %--Enforce constraints on DM commands 
     % (not needed here--just done here for stats and plotting)
-    if(any(mp.dm_ind==1)); mp.dm1 = falco_enforce_dm_constraints(mp.dm1); end
-    if(any(mp.dm_ind==2)); mp.dm2 = falco_enforce_dm_constraints(mp.dm2); end
+    if any(mp.dm_ind == 1); mp.dm1 = falco_enforce_dm_constraints(mp.dm1); end
+    if any(mp.dm_ind == 2); mp.dm2 = falco_enforce_dm_constraints(mp.dm2); end
     
     %--Update DM actuator gains for new voltages
-    if(any(mp.dm_ind==1)); mp.dm1 = falco_update_dm_gain_map(mp.dm1); end
-    if(any(mp.dm_ind==2)); mp.dm2 = falco_update_dm_gain_map(mp.dm2); end
+    if any(mp.dm_ind == 1); mp.dm1 = falco_update_dm_gain_map(mp.dm1); end
+    if any(mp.dm_ind == 2); mp.dm2 = falco_update_dm_gain_map(mp.dm2); end
     
     %--Save out regularization used.
     out.log10regHist(Itr) = cvar.log10regUsed; 
@@ -238,13 +186,12 @@ for Itr = 1:mp.Nitr
     out = falco_compute_dm_stats(mp, out, Itr);
 
     %--Calculate sensitivities to 1nm RMS of Zernike phase aberrations at entrance pupil.
-    if( isempty(mp.eval.Rsens)==false || isempty(mp.eval.indsZnoll)==false )
-        out.Zsens(:,:,Itr) = falco_get_Zernike_sensitivities(mp);
+    if ~isempty(mp.eval.Rsens) || ~isempty(mp.eval.indsZnoll)
+        out.Zsens(:, :, Itr) = falco_get_Zernike_sensitivities(mp);
     end
 
     %--REPORTING NORMALIZED INTENSITY
     if isfield(cvar, 'cMin') && mp.ctrl.flagUseModel == false
-        out.InormHist(Itr+1) = cvar.cMin;
         fprintf('Prev and New Measured NI:\t\t\t %.2e\t->\t%.2e\t (%.2f x smaller)  \n\n',...
             out.InormHist(Itr), out.InormHist(Itr+1), out.InormHist(Itr)/out.InormHist(Itr+1) );
         if ~mp.flagSim
@@ -255,21 +202,21 @@ for Itr = 1:mp.Nitr
     end
 
     %--Save out DM commands after each iteration in case the trial crashes part way through.
-    if(mp.flagSaveEachItr)
+    if mp.flagSaveEachItr
         fprintf('Saving DM commands for this iteration...')
-        if(any(mp.dm_ind==1)); DM1V = mp.dm1.V; else; DM1V = 0; end
-        if(any(mp.dm_ind==2)); DM2V = mp.dm2.V; else; DM2V = 0; end
-        if(any(mp.dm_ind==8)); DM8V = mp.dm8.V; else; DM8V = 0; end
-        if(any(mp.dm_ind==9)); DM9V = mp.dm9.V; else; DM9V = 0; end
+        if any(mp.dm_ind == 1); DM1V = mp.dm1.V; else; DM1V = 0; end
+        if any(mp.dm_ind == 2); DM2V = mp.dm2.V; else; DM2V = 0; end
+        if any(mp.dm_ind == 8); DM8V = mp.dm8.V; else; DM8V = 0; end
+        if any(mp.dm_ind == 9); DM9V = mp.dm9.V; else; DM9V = 0; end
         Nitr = mp.Nitr;
         thput_vec = mp.thput_vec;
-        fnWS = sprintf('%sws_%s_Iter%dof%d.mat',mp.path.wsInProgress, mp.runLabel, Itr, mp.Nitr);
+        fnWS = sprintf('%sws_%s_Iter%dof%d.mat', mp.path.wsInProgress, mp.runLabel, Itr, mp.Nitr);
         save(fnWS,'Nitr','Itr','DM1V','DM2V','DM8V','DM9V','out.InormHist','thput_vec','out')
         fprintf('done.\n\n')
     end
 
     %% SAVE THE TRAINING DATA OR RUN THE E-M Algorithm
-    if(mp.flagTrainModel)
+    if mp.flagTrainModel
         ev.Itr = Itr;
         mp = falco_train_model(mp,ev);
     end
@@ -280,30 +227,29 @@ end %--END OF ESTIMATION + CONTROL LOOP
 Itr = Itr + 1;
 
 %--Compute the DM surfaces
-if(any(mp.dm_ind==1)); DM1surf =  falco_gen_dm_surf(mp.dm1, mp.dm1.compact.dx, mp.dm1.compact.Ndm);  end
-if(any(mp.dm_ind==2)); DM2surf =  falco_gen_dm_surf(mp.dm2, mp.dm2.compact.dx, mp.dm2.compact.Ndm);  end
+if any(mp.dm_ind == 1); DM1surf = falco_gen_dm_surf(mp.dm1, mp.dm1.compact.dx, mp.dm1.compact.Ndm);  end
+if any(mp.dm_ind == 2); DM2surf = falco_gen_dm_surf(mp.dm2, mp.dm2.compact.dx, mp.dm2.compact.Ndm);  end
 
 %--Data to store
-if(any(mp.dm_ind==1)); out.dm1.Vall(:,:,Itr) = mp.dm1.V; end
-if(any(mp.dm_ind==2)); out.dm2.Vall(:,:,Itr) = mp.dm2.V; end
-if(any(mp.dm_ind==5)); out.dm5.Vall(:,:,Itr) = mp.dm5.V; end
-if(any(mp.dm_ind==8)); out.dm8.Vall(:,Itr) = mp.dm8.V; end
-if(any(mp.dm_ind==9)); out.dm9.Vall(:,Itr) = mp.dm9.V; end
+if any(mp.dm_ind == 1); out.dm1.Vall(:,:,Itr) = mp.dm1.V; end
+if any(mp.dm_ind == 2); out.dm2.Vall(:,:,Itr) = mp.dm2.V; end
+if any(mp.dm_ind == 8); out.dm8.Vall(:,Itr) = mp.dm8.V; end
+if any(mp.dm_ind == 9); out.dm9.Vall(:,Itr) = mp.dm9.V; end
 
 %--Calculate the core throughput (at higher resolution to be more accurate)
-[mp,thput,ImSimOffaxis] = falco_compute_thput(mp);
-if(mp.flagFiber)
+[mp, thput, ImSimOffaxis] = falco_compute_thput(mp);
+if mp.flagFiber
     mp.thput_vec(Itr) = max(thput);
 else
-    mp.thput_vec(Itr) = thput; %--record keeping
+    mp.thput_vec(Itr) = thput;
 end
 
-if(isfield(mp,'testbed') )
+if isfield(mp, 'testbed')
     out.InormHist_tb.total = out.InormHist; 
     Im_tb.Im = Im;
     Im_tb.E = zeros([size(Im),mp.Nsbp]);
     Im_tb.Iinco = zeros([size(Im),mp.Nsbp]);
-    if(~strcmpi(mp.estimator,'perfect') )
+    if ~strcmpi(mp.estimator, 'perfect')
         for si = 1:mp.Nsbp
             tmp = zeros(size(Im));
             tmp(mp.Fend.corr.mask) = Eest(:,si);
@@ -329,12 +275,12 @@ else
 end
 
 %% Save the final DM commands separately for faster reference
-if(isfield(mp,'dm1')); if(isfield(mp.dm1,'V')); out.DM1V = mp.dm1.V; end; end
-if(isfield(mp,'dm2')); if(isfield(mp.dm2,'V')); out.DM2V = mp.dm2.V; end; end
+if isfield(mp, 'dm1'); if(isfield(mp.dm1, 'V')); out.DM1V = mp.dm1.V; end; end
+if isfield(mp, 'dm2'); if(isfield(mp.dm2, 'V')); out.DM2V = mp.dm2.V; end; end
 switch upper(mp.coro)
-    case{'HLC','EHLC'}
-        if(isfield(mp.dm8,'V')); out.DM8V = mp.dm8.V;  end
-        if(isfield(mp.dm9,'V')); out.DM9V = mp.dm9.V;  end
+    case{'HLC', 'EHLC'}
+        if isfield(mp.dm8, 'V'); out.DM8V = mp.dm8.V; end
+        if isfield(mp.dm9, 'V'); out.DM9V = mp.dm9.V; end
 end
 
 %% Save out an abridged workspace
@@ -355,9 +301,8 @@ save(fnOut,'out');
 fprintf('...done.\n\n')
 
 %% Save out the data from the workspace
-if(mp.flagSaveWS)
+if mp.flagSaveWS
     clear cvar G* h* jacStruct; % Save a ton of space when storing the workspace
-    clear Gcomplex Gall Eall Eri Gri U S EriPrime IriPrime 
 
     % Don't bother saving the large 2-D, floating point maps in the workspace (they take up too much space)
     mp.P1.full.mask=1; mp.P1.compact.mask=1;
@@ -376,7 +321,7 @@ if(mp.flagSaveWS)
     mp.dm8.inf_datacube = 0;
     mp.dm9.inf_datacube = 0;
 
-    fnAll = [mp.path.ws mp.runLabel,'_all.mat'];
+    fnAll = [mp.path.ws mp.runLabel, '_all.mat'];
     disp(['Saving entire workspace to file ' fnAll '...'])
     save(fnAll);
     fprintf('done.\n\n')
--- setup/falco_init_storage_arrays.m
+++ setup/falco_init_storage_arrays.m
@@ -44,11 +44,16 @@ function out = falco_init_storage_arrays(mp)
     out.complexProjection = zeros(mp.Nitr-1, mp.Nsbp); % Metric to compare magnitude of the correction step taken to the expected one
     out.complexCorrelation = zeros(mp.Nitr-1, mp.Nsbp); % Metric to compare the morphology of the delta E-field estimated vs expected in the model
     
-    %--Intensity at each iteration
+    %--Intensity history at each iteration
     out.InormHist = zeros(mp.Nitr+1, 1); % Measured, mean raw NI in correction region of dark hole.
+    out.IrawCorrHist = zeros(mp.Nitr+1, 1); % Measured, mean raw NI in correction region of dark hole.
+    out.IrawScoreHist = zeros(mp.Nitr+1, 1); % Measured, mean raw NI in scoring region of dark hole.
     out.IestCorrHist = zeros(mp.Nitr, 1); % Mean estimated coherent NI in correction region of dark hole.
     out.IestScoreHist = zeros(mp.Nitr, 1); % Mean estimated coherent NI in scoring region of dark hole.
     out.IincoCorrHist = zeros(mp.Nitr, 1); % Mean estimated incoherent NI in correction region of dark hole.
     out.IincoScoreHist = zeros(mp.Nitr, 1); % Mean estimated incoherent NI in scoring region of dark hole.
+    
+    %--Image resolution
+    out.Fend.res = mp.Fend.res;
 
 end
