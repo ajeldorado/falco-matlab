3057fd6304fefd4798cfec07b9ce90e2a62e1723
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2021-03-03 09:18:34 -0600

    Added more fields to out structure.

--- setup/falco_init_storage_arrays.m
+++ setup/falco_init_storage_arrays.m
@@ -58,5 +58,7 @@ function out = falco_init_storage_arrays(mp)
     
     %--Image resolution
     out.Fend.res = mp.Fend.res;
+    out.Fend.corr.maskBool = mp.Fend.corr.maskBool;
+    out.Fend.score.maskBool = mp.Fend.score.maskBool;
 
 end
