9d4e51f22a633b9b72767acadbd7286b69010a34
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2021-04-01 17:12:25 -0500

    Changed a comment.
    Needed to get GitHub to start a new test run with a new commit.

--- setup/falco_compute_fpm_zwfs_coordinates.m
+++ setup/falco_compute_fpm_zwfs_coordinates.m
@@ -1,7 +1,7 @@
-% Copyright 2019, by the California Institute of Technology. ALL RIGHTS
-% RESERVED. United States Government Sponsorship acknowledged. Any
-% commercial use must be negotiated with the Office of Technology Transfer
-% at the California Institute of Technology.
+% Copyright 2018-2021, by the California Institute of Technology.
+% ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+% Any commercial use must be negotiated with the Office 
+% of Technology Transfer at the California Institute of Technology.
 % -------------------------------------------------------------------------
 %
 % FPM coordinates, [meters] and [dimensionless]
