861243081ba897ccc05d57a463007bd6d03895c2
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2021-04-12 17:48:44 -0500

    Added back simulation-only summed image generator
    - Added back in ability to parallelize computation of simulated full-bandpass images with falco_get_summed_image.m. Decided that this speed-up was too important in simulations to skip.
    - Now includes tests to make sure it gives the same result compared to the slower, separate path used to compute it in serial.

---
+++ testing/testarchive/@ImageParameters/ImageParameters.m
@@ -0,0 +1,55 @@
+%%% Paramameters Function
+%
+% This function will be called by test scripts requiring one or more of the
+% mp parameters necessary to run the test for given FALCO function/script.
+% No input is required.
+
+function [mp] = Parameters()
+%% Define Necessary Paths on Your Computer System
+
+% In this section we define and add necessary paths to FALCO.
+mp.path.falco = fileparts(fileparts(fileparts(fileparts(mfilename('fullpath'))))); % falco-matlab directory;
+addpath(genpath([mp.path.falco filesep 'setup']))
+addpath(genpath([mp.path.falco filesep 'lib']))
+addpath(genpath([mp.path.falco filesep 'config']))
+addpath(genpath([mp.path.falco filesep 'lib_external']))
+
+%% Load default model parameters
+%
+% This section runs a script found in falco repo to generate certain FALCO
+% default parameters. These parameters can be overwritten below.
+EXAMPLE_defaults_WFIRST_LC
+
+%% Overwrite default values as desired
+%
+% This section overwrites some of the default parameters needed to run
+% FALCO's main example.
+
+%%%--command line message
+mp.flagfprintf = false;
+
+%%%--Special Computational Settings
+mp.flagParfor = false; %--whether to use parfor for Jacobian calculation
+mp.flagPlot = false;
+mp.propMethodPTP = 'mft';
+
+%%% Use just 1 wavelength for initial debugging of code
+mp.fracBW = 0.10;%0.01;       %--fractional bandwidth of the whole bandpass (Delta lambda / lambda0)
+mp.Nsbp = 3;%1;            %--Number of sub-bandpasses to divide the whole bandpass into for estimation and control
+mp.Nwpsbp = 3;%1; 
+
+%% Generate the label associated with this trial
+%
+% This generates the label associated with a particular trial, not really
+% needed here.
+mp.runLabel = 'testing_label';
+
+
+%% Perform the Wavefront Sensing and Control
+%
+% This section generates parameters required to run the full wfsc
+% calculation.
+[mp, out] = falco_flesh_out_workspace(mp);
+% timerValEnd = toc;
+% disp(['Time Elapsed:  ' num2str(timerValEnd)])
+end
--- testing/testarchive/TestGetSummedImage.m
+++ testing/testarchive/TestGetSummedImage.m
@@ -1,11 +1,11 @@
 classdef TestGetSummedImage < matlab.unittest.TestCase
 %% Properties
 %
-% A presaved file with FALCO parameters was saved and is lodaded to be used
+% A presaved file with FALCO parameters was saved and is load to be used
 % by methods. In this case we only use the mp.path.falco + lib/utils to
 % addpath to utils functions to be tested.
     properties
-        mp=Parameters();
+        mp=ImageParameters();
     end
 
 %% Setup and Teardown Methods
@@ -31,11 +31,28 @@ classdef TestGetSummedImage < matlab.unittest.TestCase
 % # *test Image Size* with actal size equal to [mp.Fend.Nxi,mp.Fend.Nxi]
 % 
     methods (Test)
+        
         function testImageSize(testCase)
             import matlab.unittest.constraints.HasSize
             act = falco_get_summed_image(testCase.mp);
-            testCase.assertThat(act,HasSize([testCase.mp.Fend.Nxi testCase.mp.Fend.Nxi]))
+            testCase.assertThat(act, HasSize([testCase.mp.Fend.Nxi testCase.mp.Fend.Nxi]))
         end
          
+        function  testParallelVsSerialImageResult(testCase)
+         
+            disp('Generating image without parfor.')
+            testCase.mp.flagParfor = false;
+            imageSerial = falco_get_summed_image(testCase.mp);
+            
+            disp('Generating image with parfor.')
+            testCase.mp.flagParfor = true;
+            imageParallel = falco_get_summed_image(testCase.mp);
+            
+            absDiff = abs(imageSerial - imageParallel);
+            
+            testCase.verifyLessThan(absDiff, 1e-12)
+            
+            
+        end
     end    
 end
