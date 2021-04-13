516221798e663ccb84b4ee46ea5e5699ee8d1001
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2021-04-13 09:11:06 -0500

    Linting

--- lib/errormap/falco_gen_simple_PSD_errormap.m
+++ lib/errormap/falco_gen_simple_PSD_errormap.m
@@ -1,16 +1,16 @@
-% Copyright 2018-2021, by the California Institute of Technology. ALL RIGHTS
-% RESERVED. United States Government Sponsorship acknowledged. Any
-% commercial use must be negotiated with the Office of Technology Transfer
-% at the California Institute of Technology.
+% Copyright 2018-2021, by the California Institute of Technology.
+% ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+% Any commercial use must be negotiated with the Office 
+% of Technology Transfer at the California Institute of Technology.
 % -------------------------------------------------------------------------
 %
-% Simulate the phase error of a mirror as 1/f^(alpha) noise.
+% Generate the phase error of a mirror as 1/f^(alpha) noise.
 %
 % INPUTS
 % ------
 % N : the number of samples in each dimension. width of the output array
 % alpha : the 1/f noise power
-% mirror_figure : the peak-to-valley noise variation, in arbitrary units
+% mirror_figure : the peak-to-valley noise variation [arbitrary units]
 %
 % OUTPUTS
 % -------
@@ -20,16 +20,18 @@
 
 function out = falco_gen_simple_PSD_errormap(N, alpha, mirror_figure)
 
-aberr = rand(N);
-FT = fft2(aberr);
+    aberr = rand(N);
+    FT = fft2(aberr);
 
-x = ((0:1:N-1)-N/2)' * ones(1, N);
-y = ones(N, 1) * ((0:1:N-1)-N/2);
+    x = ((0:1:N-1)-N/2)' * ones(1, N);
+    y = ones(N, 1) * ((0:1:N-1)-N/2);
 
-envelope = fftshift(1./(x.^2+y.^2).^(alpha/4));
-envelope(1, 1) = 0; % DC component undefined, set to 0
+    envelope = fftshift(1./(x.^2+y.^2).^(alpha/4));
+    envelope(1, 1) = 0; % DC component undefined, set to 0
 
-FT = FT .* envelope;
-out = real(fftshift(ifft2(FT)));
-%out = out/(max(max(out)) - min(min(out)))*mirror_figure;
-out = out/sqrt(sum(sum(out.^2)))*mirror_figure*N;
+    FT = FT .* envelope;
+    out = real(fftshift(ifft2(FT)));
+    %out = out/(max(max(out)) - min(min(out)))*mirror_figure;
+    out = out/sqrt(sum(sum(out.^2)))*mirror_figure*N;
+    
+end
