418432a6aa4264d29f9457ceef84ec23dfec7e87
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2021-09-21 15:50:41 -0500

    New functions for azimuthal phase masks

---
+++ demo/demo_gen_azimuthal_phase_masks.m
@@ -0,0 +1,63 @@
+% Copyright 2018-2021, by the California Institute of Technology. ALL RIGHTS
+% RESERVED. United States Government Sponsorship acknowledged. Any
+% commercial use must be negotiated with the Office of Technology Transfer
+% at the California Institute of Technology.
+% -------------------------------------------------------------------------
+%
+% Demonstrate all the options for falco_gen_azimuthal_phase_mask()
+%
+
+clear
+
+inputs.type = 'staircase';  % 'vortex', 'cos', 'sectors', and 'staircase.'
+inputs.N = 200; % pixels across the 
+inputs.charge = 4; % charge of the mask (makes most sense for vortex)
+inputs.Nsteps = 6; % number of steps per 2*pi radians. For 'staircase' only
+
+%--Optional Inputs
+% inputs.centering = 'pixel';
+% inputs.xOffset = 5.5; % [pixels]
+% inputs.yOffset = -10; % [pixels]
+% inputs.clocking = 0; % [degrees]
+
+% Check staircase
+mask = falco_gen_azimuthal_phase_mask(inputs);
+figure(1); imagesc(angle(mask)); axis xy equal tight; colorbar; colormap gray; drawnow;
+
+% Check clocking
+inputs.clocking = 30; % [degrees]
+mask = falco_gen_azimuthal_phase_mask(inputs);
+figure(2); imagesc(angle(mask)); axis xy equal tight; colorbar; colormap gray; drawnow;
+
+% Check lateral offsets and rotation together
+inputs.clocking = 30; % [degrees]
+inputs.xOffset = 30.2; % [pixels]
+inputs.yOffset = -14.5; % [pixels]
+mask = falco_gen_azimuthal_phase_mask(inputs);
+figure(3); imagesc(angle(mask)); axis xy equal tight; colorbar; colormap gray; drawnow;
+
+%%
+clear inputs
+
+% Check vortex
+inputs.type = 'vortex';  % 'vortex', 'cos', 'sectors', and 'staircase.'
+inputs.N = 200; % pixels across the 
+inputs.charge = 6; % charge of the mask (makes most sense for vortex)
+mask = falco_gen_azimuthal_phase_mask(inputs);
+figure(4); imagesc(angle(mask)); axis xy equal tight; colorbar; colormap gray; drawnow;
+
+% Check sectors
+inputs.type = 'sectors';  % 'vortex', 'cos', 'sectors', and 'staircase.'
+mask = falco_gen_azimuthal_phase_mask(inputs);
+figure(5); imagesc(angle(mask)); axis xy equal tight; colorbar; colormap gray; drawnow;
+
+% Check cos
+inputs.type = 'cos';  % 'vortex', 'cos', 'sectors', and 'staircase.'
+mask = falco_gen_azimuthal_phase_mask(inputs);
+figure(6); imagesc(angle(mask)); axis xy equal tight; colorbar; colormap gray; drawnow;
+
+% Check rotated cos
+inputs.clocking = 30; % [degrees]
+inputs.type = 'cos';  % 'vortex', 'cos', 'sectors', and 'staircase.'
+mask = falco_gen_azimuthal_phase_mask(inputs);
+figure(7); imagesc(angle(mask)); axis xy equal tight; colorbar; colormap gray; drawnow;
---
+++ lib/propcustom/propcustom_mft_PtoFtoP.m
@@ -0,0 +1,92 @@
+% Copyright 2018-2021, by the California Institute of Technology. ALL RIGHTS
+% RESERVED. United States Government Sponsorship acknowledged. Any
+% commercial use must be negotiated with the Office of Technology Transfer
+% at the California Institute of Technology.
+% -------------------------------------------------------------------------
+%
+% function OUT = propcustom_mft_PtoFtoP(IN, FPM, charge, apRad, inVal, outVal, useGPU ) 
+%
+% Propagate from the pupil plane before a FPM to the
+% pupil plane after it.
+%
+% FPM must be an array with dimensions MxM, where M = lambdaOverD*(beam diameter)
+%
+% INPUTS
+% ------
+% IN: 2-D E-field at the pupil from which to propagate
+% FPM: 2-D, complex-valued array representing the FPM to use.
+% apRad: radius of the beam at the starting pupil
+% inVal: radial separation in lambda/D at which to start the Tukey window
+%        transition between coarse and fine resolution.
+% outVal: radial separation in lambda/D at which to end the Tukey window
+%         transition between coarse and fine resolution.
+% useGPU: boolean flag whether to use a GPU to speed up calculations
+%
+% OUTPUTS
+% -------
+% OUT: 2-D E-field at the next pupil plane after propagating through the
+%      FPM.
+%
+% WARNINGS
+% --------
+% 1. Use this only with infinite-extent focal plane masks. (That is,
+%    with no outer cutoff within the field of view of the array.)
+% 2. Use this only with azimuthal-only masks. Radial variation will cause
+%    problems because the same FPM array is used at both the coarse and
+%    fine resolutions.
+
+function OUT = propcustom_mft_PtoFtoP(IN, FPM, apRad, inVal, outVal, useGPU )
+
+    showPlots2debug = false; 
+
+    D = 2*apRad;
+    lambdaOverD = 4; % [samples per lambda/D]
+    
+    [NA, ~] = size(IN);
+    NB = lambdaOverD*D; 
+    
+    [X,Y] = meshgrid(-NB/2:NB/2-1);
+    RHO = sqrt(X.^2 + Y.^2);
+   
+    windowKnee = 1-inVal/outVal;
+    
+    windowMASK1 = falco_gen_Tukey4vortex(2*outVal*lambdaOverD, RHO, windowKnee) ;
+    windowMASK2 = falco_gen_Tukey4vortex(NB, RHO, windowKnee) ;
+
+    % DFT vectors 
+    x = (-NA/2:NA/2-1)/D;
+    u1 = (-NB/2:NB/2-1)/lambdaOverD;
+    u2 = (-NB/2:NB/2-1)*2*outVal/NB;
+        
+    if useGPU
+        IN = gpuArray(IN);
+        x = gpuArray(x);
+        u1 = gpuArray(u1);
+        u2 = gpuArray(u2);
+        windowMASK1 = gpuArray(windowMASK1);
+        windowMASK2 = gpuArray(windowMASK2);
+    end
+
+    if showPlots2debug; figure; imagesc(abs(IN)); axis image; colorbar; title('pupil'); end
+
+    % Full FOV, lower sampling DFT
+    FP1 = 1/(1*D*lambdaOverD)*exp(-1i*2*pi*u1'*x)*IN*exp(-1i*2*pi*x'*u1); 
+    if showPlots2debug; figure; imagesc(log10(abs(FP1).^2)); axis image; colorbar; title('Large scale DFT'); end
+
+    LP1 = 1/(1*D*lambdaOverD)*exp(-1i*2*pi*x'*u1)*(FP1.*FPM.*(1-windowMASK1))*exp(-1i*2*pi*u1'*x);
+    if showPlots2debug; figure; imagesc(abs(FP1.*(1-windowMASK1))); axis image; colorbar; title('Large scale DFT (windowed)'); end
+    
+    % Smaller FOV, finer sampling DFT
+    FP2 = 2*outVal/(1*D*NB)*exp(-1i*2*pi*u2'*x)*IN*exp(-1i*2*pi*x'*u2); 
+    if showPlots2debug; figure; imagesc(log10(abs(FP2).^2)); axis image; colorbar; title('Fine sampled DFT'); end
+
+    LP2 = 2*outVal/(1*D*NB)*exp(-1i*2*pi*x'*u2)*(FP2.*FPM.*windowMASK2)*exp(-1i*2*pi*u2'*x);        
+    if showPlots2debug; figure; imagesc(abs(FP2.*windowMASK2)); axis image; colorbar; title('Fine sampled DFT (windowed)'); end
+    OUT = LP1 + LP2;
+    if showPlots2debug; figure; imagesc(abs(OUT)); axis image; colorbar; title('Lyot plane'); end
+
+    if useGPU
+        OUT = gather(OUT);
+    end
+    
+end
