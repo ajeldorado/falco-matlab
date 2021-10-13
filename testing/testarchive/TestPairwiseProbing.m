b5a1057b496ed571f2918f35e4f9b5a33bb3197a
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2021-10-13 11:59:04 -0500

    Added class for DM constraints.

--- testing/testarchive/TestPairwiseProbing.m
+++ testing/testarchive/TestPairwiseProbing.m
@@ -10,12 +10,12 @@
 classdef TestPairwiseProbing < matlab.unittest.TestCase    
 %% Properties
 %
-% A presaved file with FALCO parameters was saved and is lodaded to be used
+% A presaved file with FALCO parameters was saved and is loaded to be used
 % by methods. In this case we only use the mp.path.falco + lib/utils to
 % addpath to utils functions to be tested.
-    properties
-        mp=Parameters();
-    end
+%     properties
+%         mp=Parameters();
+%     end
 
 %% Setup and Teardown Methods
 %
@@ -23,20 +23,20 @@ classdef TestPairwiseProbing < matlab.unittest.TestCase
 %
     methods (TestClassSetup)
         function addPath(testCase)
-            addpath(genpath([testCase.mp.path.falco filesep 'config']));
-            addpath(genpath([testCase.mp.path.falco filesep 'lib']));
-            addpath(genpath([testCase.mp.path.falco filesep 'lib_external']));
-            addpath(genpath([testCase.mp.path.falco filesep 'models']));
-            addpath(genpath([testCase.mp.path.falco filesep 'setup']));
+            addpath(genpath('../../config'));
+            addpath(genpath('../../lib'));
+            addpath(genpath('../../lib_external'));
+            addpath(genpath('../../models'));
+            addpath(genpath('../../setup'));
         end
     end
     methods (TestClassTeardown)
         function removePath(testCase)
-            rmpath(genpath([testCase.mp.path.falco filesep 'config']));
-            rmpath(genpath([testCase.mp.path.falco filesep 'lib']));
-            rmpath(genpath([testCase.mp.path.falco filesep 'lib_external']));
-            rmpath(genpath([testCase.mp.path.falco filesep 'models']));
-            rmpath(genpath([testCase.mp.path.falco filesep 'setup']));
+            rmpath(genpath('../../config'));
+            rmpath(genpath('../../lib'));
+            rmpath(genpath('../../lib_external'));
+            rmpath(genpath('../../models'));
+            rmpath(genpath('../../setup'));
         end
     end
     
@@ -82,7 +82,7 @@ classdef TestPairwiseProbing < matlab.unittest.TestCase
             [mp, out] = falco_flesh_out_workspace(mp);
             N = size(mp.P1.full.E, 1);
             alpha = 2.5;
-            mirror_figure = 1e-9;
+            mirror_figure = 1e-10;
             errormap = falco_gen_simple_PSD_errormap(N, alpha, mirror_figure);
             mp.P1.full.E = exp(2*pi*1j/mp.lambda0*errormap);
             mp.P1.compact.E = exp(2*pi*1j/mp.lambda0*errormap);
