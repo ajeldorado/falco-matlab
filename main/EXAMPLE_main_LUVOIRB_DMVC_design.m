f223c49761fa260d2cfb983bb1fa6ecc13d79dbc
Jorge <jdllops@gmail.com>
2021-10-18 10:37:44 -0700

    Changes

--- main/EXAMPLE_main_LUVOIRB_DMVC_design.m
+++ main/EXAMPLE_main_LUVOIRB_DMVC_design.m
@@ -11,19 +11,21 @@ clear
 %% Step 1: Define Necessary Paths on Your Computer System
 
 %--Required packages are FALCO and PROPER. 
-% Add FALCO to the MATLAB path with the command:  addpath(genpath(full_path_to_falco)); savepath;
-% Add PROPER to the MATLAB path with the command:  addpath(full_path_to_proper); savepath;
+full_path_to_falco = '/Users/jllopsay/Documents/GitHub/falco-matlab';
+full_path_to_proper = '/Users/jllopsay/Documents/MATLAB/PROPER';
+addpath(genpath(full_path_to_falco)); savepath;
+addpath(full_path_to_proper); savepath;
 
 %%--Output Data Directories (Comment these lines out to use defaults within falco-matlab/data/ directory.)
-% mp.path.config = ; %--Location of config files and minimal output files. Default is [mp.path.falco filesep 'data' filesep 'brief' filesep]
-% mp.path.ws = ; % (Mostly) complete workspace from end of trial. Default is [mp.path.falco filesep 'data' filesep 'ws' filesep];
-% mp.flagSaveWS = false;  %--Whether to save out entire (large) workspace at the end of trial. Default is false
+mp.path.config = '/Users/jllopsay/Documents/GitHub/falco-matlab/config'; %--Location of config files and minimal output files. Default is [mp.path.falco filesep 'data' filesep 'brief' filesep]
+mp.path.ws = '/Users/jllopsay/Documents/GitHub/falco-matlab/data/ws'; % (Mostly) complete workspace from end of trial. Default is [mp.path.falco filesep 'data' filesep 'ws' filesep];
+mp.flagSaveWS = false;  %--Whether to save out entire (large) workspace at the end of trial. Default is false
 
 
 %% Step 2: Load default model parameters
 
 EXAMPLE_defaults_LUVOIRB_VC_design
-
+mp.flagJitter = false;
 
 %% Step 3: Overwrite default values as desired
 
