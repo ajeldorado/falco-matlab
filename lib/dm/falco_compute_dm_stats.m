51c966247f51142afc2a18795ac56274d9a9ba5c
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2021-10-20 12:54:57 -0500

    Deprecated maxAbsV and maxAbsdV

--- lib/dm/falco_compute_dm_stats.m
+++ lib/dm/falco_compute_dm_stats.m
@@ -16,9 +16,9 @@ if(any(mp.dm_ind==1)); DM1surf =  falco_gen_dm_surf(mp.dm1, mp.dm1.compact.dx, m
 if(any(mp.dm_ind==2)); DM2surf =  falco_gen_dm_surf(mp.dm2, mp.dm2.compact.dx, mp.dm2.compact.Ndm);  end
 
 %--Compute coordinates
-if(any(mp.dm_ind==1))
+if any(mp.dm_ind == 1)
     Ndm = length(DM1surf);
-elseif(any(mp.dm_ind==2))
+elseif any(mp.dm_ind == 2)
     Ndm = length(DM2surf);
 end
 dx_dm = mp.P2.compact.dx / mp.P2.D; %--Normalized dx. Units of pupil diameters.
@@ -33,39 +33,41 @@ RS = sqrt(XS.^2 + YS.^2);
 rmsSurfInd = find(RS>=mp.P1.IDnorm/2 & RS<=OD_pup/2);
 
 %--Calculate and report updated P-V DM voltages.
-if(any(mp.dm_ind==1))
-    out.dm1.Vpv(Itr) = (max(max(mp.dm1.V))-min(min(mp.dm1.V)));
-    Nrail1 = length(find( (mp.dm1.V <= -mp.dm1.maxAbsV) | (mp.dm1.V >= mp.dm1.maxAbsV) ));
+if any(mp.dm_ind == 1)
+    out.dm1.Vpv(Itr) = max(mp.dm1.V(:)) - min(mp.dm1.V(:));
+    Vtotal1 = mp.dm1.V + mp.dm1.biasMap;
+    Nrail1 = length(find((Vtotal1 <= mp.dm1.Vmin) | (Vtotal1 >= mp.dm1.Vmax)));
     fprintf(' DM1 P-V in volts: %.3f\t\t%d/%d (%.2f%%) railed actuators \n', out.dm1.Vpv(Itr), Nrail1, mp.dm1.NactTotal, 100*Nrail1/mp.dm1.NactTotal); 
-    if(size(mp.dm1.tied,1)>0);  fprintf(' DM1 has %d pairs of tied actuators.\n', size(mp.dm1.tied,1));  end  
+    if(size(mp.dm1.tied, 1) > 0);  fprintf(' DM1 has %d pairs of tied actuators.\n', size(mp.dm1.tied,1));  end  
 end
-if(any(mp.dm_ind==2))
-    out.dm2.Vpv(Itr) = (max(max(mp.dm2.V))-min(min(mp.dm2.V)));
-    Nrail2 = length(find( (mp.dm2.V <= -mp.dm2.maxAbsV) | (mp.dm2.V >= mp.dm2.maxAbsV) ));
+if any(mp.dm_ind == 2)
+    out.dm2.Vpv(Itr) = max(mp.dm2.V(:)) - min(mp.dm2.V(:));
+    Vtotal2 = mp.dm2.V + mp.dm2.biasMap;
+    Nrail2 = length(find((Vtotal2 <= mp.dm2.Vmin) | (Vtotal2 >= mp.dm2.Vmax)));
     fprintf(' DM2 P-V in volts: %.3f\t\t%d/%d (%.2f%%) railed actuators \n', out.dm2.Vpv(Itr), Nrail2, mp.dm2.NactTotal, 100*Nrail2/mp.dm2.NactTotal); 
-    if(size(mp.dm2.tied,1)>0);  fprintf(' DM2 has %d pairs of tied actuators.\n', size(mp.dm2.tied,1));  end 
+    if(size(mp.dm2.tied , 1) > 0);  fprintf(' DM2 has %d pairs of tied actuators.\n', size(mp.dm2.tied,1));  end 
 end
-if(any(mp.dm_ind==8))
+if any(mp.dm_ind == 8)
     out.dm8.Vpv(Itr) = (max(max(mp.dm8.V))-min(min(mp.dm8.V)));
     Nrail8 = length(find( (mp.dm8.V <= mp.dm8.Vmin) | (mp.dm8.V >= mp.dm8.Vmax) ));
     fprintf(' DM8 P-V in volts: %.3f\t\t%d/%d (%.2f%%) railed actuators \n', out.dm8.Vpv(Itr), Nrail8,mp.dm8.NactTotal,100*Nrail8/mp.dm8.NactTotal); 
 end
-if(any(mp.dm_ind==9))
+if any(mp.dm_ind == 9)
     out.dm9.Vpv(Itr) = (max(max(mp.dm9.V))-min(min(mp.dm9.V)));
     Nrail9 = length(find( (mp.dm9.V <= mp.dm9.Vmin) | (mp.dm9.V >= mp.dm9.Vmax) ));
     fprintf(' DM9 P-V in volts: %.3f\t\t%d/%d (%.2f%%) railed actuators \n', out.dm9.Vpv(Itr), Nrail9,mp.dm9.NactTotal,100*Nrail9/mp.dm9.NactTotal); 
 end
 
 %--Calculate and report updated RMS DM surfaces.
-if(any(mp.dm_ind==1))
+if any(mp.dm_ind == 1)
     out.dm1.Spv(Itr) = max(DM1surf(:))-min(DM1surf(:));
     out.dm1.Srms(Itr) = falco_rms(DM1surf(rmsSurfInd));
     fprintf('RMS surface of DM1 = %.1f nm\n', 1e9*out.dm1.Srms(Itr))
 end
-if(any(mp.dm_ind==2))
+if any(mp.dm_ind == 2)
     out.dm2.Spv(Itr) = max(DM2surf(:))-min(DM2surf(:));
     out.dm2.Srms(Itr) = falco_rms(DM2surf(rmsSurfInd));
     fprintf('RMS surface of DM2 = %.1f nm\n', 1e9*out.dm2.Srms(Itr))
 end
 
-end %--END OF FUNCTION
+end
