e0c910fc5198869f6c67d4db7becc4081ba44b7f
hcit <phillip.poon@jpl.nasa.gov>
2022-04-15 09:33:26 -0700

    added comment

--- lib/@ProbeSchedule/ProbeSchedule.m
+++ lib/@ProbeSchedule/ProbeSchedule.m
@@ -11,6 +11,7 @@ classdef ProbeSchedule
     properties
         xOffsetVec int8 % Vector of x-offsets (one value per WFSC iteration) of the probe center from the DM grid center [actuators]
         yOffsetVec int8 % Vector of x-offsets (one value per WFSC iteration) of the probe center from the DM grid center [actuators]
+        % if defined, InormProbeVec overrides the automated probe intensity
         InormProbeVec double {mustBePositive} %= 1e-6*ones(10, 1) % Vector of the desired normalized intensity of the probes at each WFSC iteration 
    end
    
