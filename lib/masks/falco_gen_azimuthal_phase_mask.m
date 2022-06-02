2f92ced3f0decf6dababea2cebc27c9dfd53d7b3
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2022-06-02 15:06:29 -0500

    Added besselzero function. Cleaned up phase mask demo script

--- lib/masks/falco_gen_azimuthal_phase_mask.m
+++ lib/masks/falco_gen_azimuthal_phase_mask.m
@@ -192,7 +192,7 @@ function mask = falco_gen_azimuthal_phase_mask(inputs)
 
             
         otherwise
-            validOptions = "Valid options are 'vortex', 'cos', 'sectors', and 'staircase.";
+            validOptions = "Valid options are 'vortex', 'cos', 'sectors', 'staircase', 'classicalwrapped', 'frenchwrapped', and 'mcmc6'.";
             error('%s is not a valid option for inputs.type. \n%s', inputs.type, validOptions)
     end
 
@@ -252,50 +252,50 @@ function [pt,rvec,qvec] = polarTransform(input_image, center_vec, rmax, numRadPt
         pt = interp2(xvals,yvals,double(input_image),xComp,yComp,method);
 end
 
-function x=besselzero(n,k,kind)
-        k3=3*k;
-        x=zeros(k3,1);
-        for j=1:k3
-            % Initial guess of zeros 
-            x0=1+sqrt(2)+(j-1)*pi+n+n^0.4;
-            % Do Halley's method
-            x(j)=findzero(n,x0,kind);
-            if x(j)==inf
-                error('Bad guess.');
-            end
-        end
-        x=sort(x);
-        dx=[1;abs(diff(x))];
-        x=x(dx>1e-8);
-        x=x(1:k);
-        function x=findzero(n,x0,kind)
-        n1=n+1;     n2=n*n;
-        % Tolerance
-        tol=1e-12;
-        % Maximum number of times to iterate
-        MAXIT=100;
-        % Initial error
-        err=1;
-        iter=0;
-        while abs(err)>tol & iter<MAXIT
-            switch kind
-                case 1
-                    a=besselj(n,x0);    
-                    b=besselj(n1,x0);   
-                case 2
-                    a=bessely(n,x0);
-                    b=bessely(n1,x0);
-            end
-            x02=x0*x0;
-            err=2*a*x0*(n*a-b*x0)/(2*b*b*x02-a*b*x0*(4*n+1)+(n*n1+x02)*a*a);
-            x=x0-err;
-            x0=x;
-            iter=iter+1;
-        end
-        if iter>MAXIT-1
-            warning('Failed to converge to within tolerance. ',...
-                    'Try a different initial guess');
-            x=inf;    
-        end
-        end
-end
+% function x = besselzero(n,k,kind)
+%         k3=3*k;
+%         x=zeros(k3,1);
+%         for j=1:k3
+%             % Initial guess of zeros 
+%             x0=1+sqrt(2)+(j-1)*pi+n+n^0.4;
+%             % Do Halley's method
+%             x(j)=findzero(n,x0,kind);
+%             if x(j)==inf
+%                 error('Bad guess.');
+%             end
+%         end
+%         x=sort(x);
+%         dx=[1;abs(diff(x))];
+%         x=x(dx>1e-8);
+%         x=x(1:k);
+%         function x=findzero(n,x0,kind)
+%         n1=n+1;     n2=n*n;
+%         % Tolerance
+%         tol=1e-12;
+%         % Maximum number of times to iterate
+%         MAXIT=100;
+%         % Initial error
+%         err=1;
+%         iter=0;
+%         while abs(err)>tol & iter<MAXIT
+%             switch kind
+%                 case 1
+%                     a=besselj(n,x0);    
+%                     b=besselj(n1,x0);   
+%                 case 2
+%                     a=bessely(n,x0);
+%                     b=bessely(n1,x0);
+%             end
+%             x02=x0*x0;
+%             err=2*a*x0*(n*a-b*x0)/(2*b*b*x02-a*b*x0*(4*n+1)+(n*n1+x02)*a*a);
+%             x=x0-err;
+%             x0=x;
+%             iter=iter+1;
+%         end
+%         if iter>MAXIT-1
+%             warning('Failed to converge to within tolerance. ',...
+%                     'Try a different initial guess');
+%             x=inf;    
+%         end
+%         end
+% end
