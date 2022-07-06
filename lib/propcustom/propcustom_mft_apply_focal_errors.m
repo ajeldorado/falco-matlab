4d682648415cf945a7c1c392c377915191d151de
A.J. Riggs <aj.riggs@jpl.nasa.gov>
2022-07-06 15:32:27 -0500

    focal errors: Added docstrings

--- lib/propcustom/propcustom_mft_apply_focal_errors.m
+++ lib/propcustom/propcustom_mft_apply_focal_errors.m
@@ -8,10 +8,17 @@
 % Propates from the pupil before the focal plane, applies the error map,
 % then back propagates to the original pupil plane.
 %
-% resErrorMap : pixels per lambda/D
-% 
+% INPUTS
+% Epupil : 2-D array of the electric field at the pupil from which to start
+% EerrorMap : 2-D arry of the complex-valued errors to multiply at the focal plane 
+% resErrorMap : Resolution of the error map in pixels per lambda0/D.
+% beamDiamPix : Number of pixels across the beam diameter in the pupil.
+%
+% OUTPUTS
+% Epupil : 2-D E-field array back at the original pupil plane but
+%   containing the errors as applied at the focal plane
 
-function EpupNew = propcustom_mft_apply_focal_errors(Epupil, EerrorMap, resErrorMap, beamDiamPix)
+function Epupil = propcustom_mft_apply_focal_errors(Epupil, EerrorMap, resErrorMap, beamDiamPix)
     
     NarrayPupil = size(Epupil, 1);
     dx = 1/beamDiamPix;
@@ -28,6 +35,6 @@ function EpupNew = propcustom_mft_apply_focal_errors(Epupil, EerrorMap, resError
     Efocus = propcustom_mft_PtoF(Epupil, fl, lambda, dx, dxi, Nxi, deta, Neta);
     
     % Backwards MFT to the original pupil plane
-    EpupNew = propcustom_mft_FtoP(Efocus .* EerrorMap, -fl, lambda, dxi, deta, dx, NarrayPupil);
+    Epupil = propcustom_mft_FtoP(Efocus .* EerrorMap, -fl, lambda, dxi, deta, dx, NarrayPupil);
 
 end
--- lib/propcustom/propcustom_mft_apply_focal_errors_babinet.m
+++ lib/propcustom/propcustom_mft_apply_focal_errors_babinet.m
@@ -1,4 +1,4 @@
-% Copyright 2018-2020, by the California Institute of Technology. ALL RIGHTS
+% Copyright 2018, by the California Institute of Technology. ALL RIGHTS
 % RESERVED. United States Government Sponsorship acknowledged. Any
 % commercial use must be negotiated with the Office of Technology Transfer
 % at the California Institute of Technology.
@@ -9,8 +9,15 @@
 % Propates from the pupil before the focal plane, applies the error map,
 % then back propagates to the original pupil plane.
 %
-% resErrorMap : pixels per lambda/D
-% 
+% INPUTS
+% Epupil : 2-D array of the electric field at the pupil from which to start
+% EerrorMap : 2-D arry of the complex-valued errors to multiply at the focal plane 
+% resErrorMap : Resolution of the error map in pixels per lambda0/D.
+% beamDiamPix : Number of pixels across the beam diameter in the pupil.
+%
+% OUTPUTS
+% Epupil : 2-D E-field array back at the original pupil plane but
+%   containing the errors as applied at the focal plane
 
 function Epupil = propcustom_mft_apply_focal_errors_babinet(Epupil, EerrorMap, resErrorMap, beamDiamPix)
     
