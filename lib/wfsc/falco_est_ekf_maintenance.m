b7a294e89b6db72553b52699f2476672ab75f849
sfr348 <sredmond@princeton.edu>
2022-07-13 17:33:11 -0700

    fixed dither/drift typos in drift injection and falco_est.  Also added method to get control vector to pass to estimate function and added ekf estimate function.

--- lib/wfsc/falco_est_ekf_maintenance.m
+++ lib/wfsc/falco_est_ekf_maintenance.m
@@ -16,7 +16,7 @@ end
 
 % Initialize EKF
 if Itr == 1
-    % TODO: jacStruct doesn't get passer to estimators?
+    
     [mp, ev, jacStruct] = initialize_ekf_maintenance(mp, ev, jacStruct);
 end
 
@@ -38,8 +38,8 @@ end
 
 %--Store the initial DM commands
 % sfr: what is this doing
-if any(mp.dm_ind == 1);  DM1Vnom = mp.dm1.V;  else; DM1Vnom = zeros(size(mp.dm1.V)); end % The 'else' block would mean we're only using DM2
-if any(mp.dm_ind == 2);  DM2Vnom = mp.dm2.V;  else; DM2Vnom = zeros(size(mp.dm2.V)); end % The 'else' block would mean we're only using DM1
+% if any(mp.dm_ind == 1);  DM1Vnom = mp.dm1.V;  else; DM1Vnom = zeros(size(mp.dm1.V)); end % The 'else' block would mean we're only using DM2
+% if any(mp.dm_ind == 2);  DM2Vnom = mp.dm2.V;  else; DM2Vnom = zeros(size(mp.dm2.V)); end % The 'else' block would mean we're only using DM1
 
 % Initialize output arrays
 ev.imageArray = zeros(mp.FendNeta, mp.Fend.Nxi, 1, mp.Nsbp);
@@ -50,10 +50,6 @@ ev.Im = zeros(mp.Fend.Neta, mp.Fend.Nxi);
 if whichDM == 1;  ev.dm1.Vall = zeros(mp.dm1.Nact, mp.dm1.Nact, 1+2*Npairs, mp.Nsbp);  end
 if whichDM == 2;  ev.dm2.Vall = zeros(mp.dm2.Nact, mp.dm2.Nact, 1+2*Npairs, mp.Nsbp);  end
 
-%% Initialize items
-if mp.Itr == 1
-    mp = initialize_ekf_maintenance(mp);
-end
 
 %% Get Drift Command
 mp = falco_drift_injection(mp);
@@ -61,31 +57,140 @@ mp = falco_drift_injection(mp);
 %% Get dither command
 % Is this if else necessary?
 
-if any(mp.dm_ind == 1);  DM1Vdither = normrnd(0,ev.dither,[mp.dm1.Nact mp.dm1.Nact]);  else; DM1Vdither = zeros(size(mp.dm1.V)); end % The 'else' block would mean we're only using DM2
-if any(mp.dm_ind == 2);  DM2Vdither = normrnd(0,ev.dither,[mp.dm1.Nact mp.dm1.Nact]);  else; DM2Vdither = zeros(size(mp.dm2.V)); end % The 'else' block would mean we're only using DM1
-
+if any(mp.dm_ind == 1);  DM1Vdither = normrnd(0,ev.dither,[mp.dm1.Nact mp.dm1.Nact]); else; DM1Vdither = zeros(size(mp.dm1.V)); end % The 'else' block would mean we're only using DM2
+if any(mp.dm_ind == 2);  DM2Vdither = normrnd(0,ev.dither,[mp.dm1.Nact mp.dm1.Nact]); else; DM2Vdither = zeros(size(mp.dm2.V)); end % The 'else' block would mean we're only using DM1
 
 %% Set total command for estimator image
 % TODO: need to save these commands for each iteration separately
-% TODO: what is controller command?
+% TODO: what is controller command? - mp.dm1.dV (make sure sign is right)
 
-mp.dm1 = falco_set_constrained_voltage(mp.dm1, mp.dm1.V_dz + DM1Vdither + DM1Vdrift + DM1V_controller_result);
-mp.dm2 = falco_set_constrained_voltage(mp.dm2, mp.dm2.V_dz + DM2Vdither + DM1Vdrift + DM2V_controller_result);
+mp.dm1 = falco_set_constrained_voltage(mp.dm1, mp.dm1.V_dz + mp.dm1.Vdrift + DM1Vdither + mp.dm1.dV);
+mp.dm2 = falco_set_constrained_voltage(mp.dm2, mp.dm2.V_dz + mp.dm2.Vdrift + DM2Vdither + mp.dm2.dV);
+
+dither = get_dm_command_vector(mp,DM1Vdither, DM2Vdither);
+if Itr > 1
+    efc_command = get_dm_command_vector(mp,mp.dm1.dV, mp.dm2.dV);
+else
+    efc_command = 0*dither;
+    mp.dm1.dV = zeros(size(DM1Vdither));
+    mp.dm2.dV = zeros(size(DM1Vdither));
+end
+% TODO: check sign on efc command
+closed_loop_command = dither + efc_command;
 
 %% Get images
 
-
+image = zeros(length(mp.Fend.score.mask),mp.Nsbp);
 for iSubband = 1:mp.Nsbp
-    I0 = falco_get_sbp_image(mp, iSubband) * tb.info.sbp_texp(si) * tb.info.PSFpeaks(si);
+    I0 = falco_get_sbp_image(mp, iSubband) * ev.peak_psf;
+    image(:,iSubband) = I0(mp.Fend.score.mask);
 
 
 end
 
 %% Perform the estimation
+ekf_estimate(mp,ev,closed_loop_command);
+% for iSubband = 1:mp.Nsbp
+% 
+% 
+% end
 
-for iSubband = 1:mp.Nsbp
+end
+
+function comm_vector = get_dm_command_vector(mp,command1, command2)
+
+if any(mp.dm_ind == 1);  comm1_arr = command1(:); comm1 = comm1_arr(mp.dm1.act_ele);  else; comm1 = []; end % The 'else' block would mean we're only using DM2
+if any(mp.dm_ind == 2);  comm2_arr = command2(:); comm2 = comm2_arr(mp.dm2.act_ele);  else; comm2 = []; end
+comm_vector = [comm1;comm2];
+end
+
+
+function [ev] = ekf_estimate(mp, ev, closed_loop_command)
+
+
+% if ev.Itr == 0
+%     closed_loop_command = dither + periodic_dm_shift;
+% else
+%     closed_loop_command = -controller.corrections(:,end) + dither + periodic_dm_shift;
+% end
+
+%% Estimation part. All EKFs are avanced in parallel
+% e_scaling = zeros(mp.Nsbp,1);
+
+
+for si = 1:1:mp.Nsbp
+
+    %--Estimate of the closed loop electric field:
+    x_hat_CL = ev.x_hat(:,si) + (jacStruct.G_tot(:,:,si).'*ev.e_scaling(si))*sqrt(ev.exposure_time_coron)*closed_loop_command;
+
+    %--Estimate of the measurement:
+    y_hat = x_hat_CL(1:ev.SS:end).^2 + x_hat_CL(2:ev.SS:end).^2 + (mp.dark_current*ev.exposure_time_coron);
+
+    ev.R(ev.R_indices) = reshape(y_hat + (ev.read_noise)^2,size(ev.R(ev.R_indices)));
+
+    ev.H(ev.H_indices) = 2 * x_hat_CL;
+%     H_T = H.transpose(0,2,1);
+    % TODO: check that this transpose is correct
+    H_T = permute(ev.H,[2,1,3]);
+
+    ev.P(:,:,:,si) = ev.P(:,:,:,si) + ev.Q(:,:,:,si);
+   
+    P_H_T = pagemtimes(ev.P(:,:,:,si), H_T);
+    S = pagemtimes(ev.H, P_H_T) + ev.R;
+    S_inv = pageinv(S) ;%does this need to be a pinv?
+
+    % S_inv = np.linalg.pinv(S)
+    K = pagemtimes(P_H_T, S_inv);
+    ev.P(:,:,:,si) = ev.P(:,:,:,si) - pagemtimes(P_H_T, permute(K,[2,1,3]));
+    
+%     EKF correction:
+%     dx_hat = pagemtimes(K, (y_measured(:,si) - y_hat).reshape((-1,ev.BS/ev.SS,1))).reshape(-1);
+%     dx_hat = permute(K,[1,3,2])*(y_measured(:,si) - y_hat);%.reshape((-1,ev.BS/ev.SS,1))).reshape(-1);
+    dy = (y_measured(:,si) - y_hat);
+    
+    dy_hat_stacked  = zeros(size(K));
+    dy_hat_stacked(1,:,:) = dy.';
+    dy_hat_stacked(2,:,:) = dy.';
+    
+    dx_hat_stacked = K.*dy_hat_stacked;
+
+    dx_hat = zeros(size(x_hat_CL));
+    dx_hat(1:ev.SS:end) = dx_hat_stacked(1,:,:);
+    dx_hat(2:ev.SS:end) = dx_hat_stacked(2,:,:);
+
+
+    ev.x_hat(:,si) = ev.x_hat(:,si) + dx_hat;
+%     
+%     % Convert E_hat to contrast units:
+%     E_hat = (ev.x_hat(:,si)(::SS) + complex(0,1)*ev.x_hat(wavelength)(1::SS))/(ev.e_scaling(wavelength) * np.sqrt(ev.exposure_time_coron)); %Estimate of the electric field from EKF state estimate
+%              
+%     
+%     % Update based on scaling factors
+% %             x_hat(wavelength) *= e_field_scale_factors(wavelength)(-1)
+% %             E_hat *= e_field_scale_factors(wavelength)(-1)
+%     
+%     E = hcipy.Field(np.zeros(wfsc_utils.focal_grid.size, dtype='complex'), wfsc_utils.focal_grid);
+%     E(dark_zone) = E_hat;
+%     E_estimates(wavelength) = E;
+%     
+
+end
+
 
 
 end
 
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/wfsc/initialize_ekf_maintenance.m
+++ lib/wfsc/initialize_ekf_maintenance.m
@@ -5,13 +5,22 @@ function [mp, ev, jacStruct] = initialize_ekf_maintenance(mp,ev, jacStruct)
 % intensity
 ev.peak_psf_counts = zeros(mp.Nsbp);
 
+%     sbp_texp  = tb.info.sbp_texp(si);% Exposure time for each sub-bandpass (seconds)
+%     numCoadds = tb.info.sbp_numCoadds(si);% Number of coadds to use for each sub-bandpass
+%     PSFpeak   = tb.info.PSFpeaks(si);% counts per second 
+%     PSFpeak_counts = PSFpeak*sbp_texp*numCoadds;
+
 for iSubband = 1:mp.Nsbp
-    if mp.flagSim
-        ev.peak_psf_counts(si) = tb.info.sbp_texp(si)*tb.info.PSFpeaks(si);
-        ev.e_scaling = sqrt(tb.info.PSFpeaks(si));
+    if ~mp.flagSim
+        % potentially set mp.detector.peakFluxVec(si) * mp.detector.tExpUnprobedVec(si) set to mp.tb.info.sbp_texp(si)*mp.tb.info.PSFpeaks(si);
+        % to have cleaner setup
+        ev.peak_psf_counts(si) = mp.tb.info.sbp_texp(si)*mp.tb.info.PSFpeaks(si);
+        ev.e_scaling = sqrt(mp.tb.info.PSFpeaks(si));
     else
-        ev.peak_psf_counts(si) = falco_get_sim_sbp_peak_counts(mp, iSubband);
-%         ev.e_scaling = abs(E)
+        % TODO: update this
+        ev.peak_psf_counts(si) = mp.detector.peakFluxVec(si) * mp.detector.tExpUnprobedVec(si); %[counts/px/s]
+%         ev.peak_psf_counts(si) = falco_get_sim_sbp_peak_counts(mp, iSubband);
+        ev.e_scaling = sqrt(mp.detector.peakFluxVec(si));
     end
 
 end
@@ -45,7 +54,7 @@ G2 = zeros(2*size(jacStruct.G1,1),mp.dm2.Nele,Nsbp);
 for iSubband = 1:mp.Nsbp
     
     if any(mp.dm_ind == 1)
-        G1_comp = jacStruct.G1[:,:,iSubband];
+        G1_comp = jacStruct.G1(:,:,iSubband);
         G1_split = zeros(2*size(jacStruct.G1,1),mp.dm1.Nele);
         G1_split(1:2:end,:) = real(G1_comp);
         G1_split(2:2:end,:) = imag(G1_comp);
@@ -152,64 +161,68 @@ for iSubband = 1:1:mp.Nsbp
  end
 
 end
+
+
+
 % TODO: these probable don't do what I want them to do right now
-function subbandImage = falco_get_sim_sbp_peak_counts(mp, iSubband)
 
-    %--Compute the DM surfaces outside the full model to save time
-    if any(mp.dm_ind == 1); mp.dm1.surfM = falco_gen_dm_surf(mp.dm1, mp.dm1.dx, mp.dm1.NdmPad); end
-    if any(mp.dm_ind == 2); mp.dm2.surfM = falco_gen_dm_surf(mp.dm2, mp.dm2.dx, mp.dm2.NdmPad); end
-    if any(mp.dm_ind == 9); mp.dm9.phaseM = falco_dm_surf_from_cube(mp.dm9, mp.dm9); end
-
-    %--Loop over all wavelengths and polarizations
-    Npol = length(mp.full.pol_conds);
-    indexComboArray = allcomb(1:mp.Nwpsbp, 1:Npol, 1:mp.star.count).'; % dimensions: [3 x mp.Nwpsbp*Npol*mp.star.count ]
-    Ncombos = size(indexComboArray, 2);
-
-    if mp.flagParfor
-        parfor iCombo = 1:Ncombos
-            Iall{iCombo} = falco_compute_subband_image_peak(mp, indexComboArray, iCombo, iSubband);
-        end
-    else
-        for iCombo = Ncombos:-1:1
-            Iall{iCombo} = falco_compute_subband_image_peak(mp, indexComboArray, iCombo, iSubband);
-        end
-    end
-
-    %--Apply the spectral weights and sum
-    subbandImage = 0; 
-    for iCombo = 1:Ncombos
-        subbandImage = subbandImage + Iall{iCombo};  
-    end
-    
-    if mp.flagImageNoise
-        subbandImage = falco_add_noise_to_subband_image(mp, subbandImage, iSubband);
-    end
-
-
-end %--END OF FUNCTION
-
-
-function Iout = falco_compute_subband_image_peak(mp, indexComboArray, iCombo, iSubband)
-
-    % Generate the weighted, normalized intensity image for a single
-    % wavelength, polarization, and star in the specified subband.
-
-    % Extract indices
-    iWavelength = indexComboArray(1, iCombo);
-    iPol = indexComboArray(2, iCombo);
-    iStar = indexComboArray(3, iCombo);
-
-    % Compute E-field
-    modvar = ModelVariables;
-    modvar.whichSource = 'star';
-    modvar.sbpIndex = iSubband;
-    modvar.wpsbpIndex = iWavelength;
-    modvar.starIndex = iStar;
-    mp.full.polaxis = mp.full.pol_conds(iPol); % used only in PROPER full models
-    Estar = model_full(mp, modvar);
-
-    % Apply wavelength weight within subband.
-    % Assume polarizations are evenly weighted.
-    Iout = length(mp.full.pol_conds) * abs(Estar).^2;
-    
-end %--END OF FUNCTION
+% function subbandImage = falco_get_sim_sbp_peak_counts(mp, iSubband)
+% 
+%     %--Compute the DM surfaces outside the full model to save time
+%     if any(mp.dm_ind == 1); mp.dm1.surfM = falco_gen_dm_surf(mp.dm1, mp.dm1.dx, mp.dm1.NdmPad); end
+%     if any(mp.dm_ind == 2); mp.dm2.surfM = falco_gen_dm_surf(mp.dm2, mp.dm2.dx, mp.dm2.NdmPad); end
+%     if any(mp.dm_ind == 9); mp.dm9.phaseM = falco_dm_surf_from_cube(mp.dm9, mp.dm9); end
+% 
+%     %--Loop over all wavelengths and polarizations
+%     Npol = length(mp.full.pol_conds);
+%     indexComboArray = allcomb(1:mp.Nwpsbp, 1:Npol, 1:mp.star.count).'; % dimensions: [3 x mp.Nwpsbp*Npol*mp.star.count ]
+%     Ncombos = size(indexComboArray, 2);
+% 
+%     if mp.flagParfor
+%         parfor iCombo = 1:Ncombos
+%             Iall{iCombo} = falco_compute_subband_image_peak(mp, indexComboArray, iCombo, iSubband);
+%         end
+%     else
+%         for iCombo = Ncombos:-1:1
+%             Iall{iCombo} = falco_compute_subband_image_peak(mp, indexComboArray, iCombo, iSubband);
+%         end
+%     end
+% 
+%     %--Apply the spectral weights and sum
+%     subbandImage = 0; 
+%     for iCombo = 1:Ncombos
+%         subbandImage = subbandImage + Iall{iCombo};  
+%     end
+%     
+%     if mp.flagImageNoise
+%         subbandImage = falco_add_noise_to_subband_image(mp, subbandImage, iSubband);
+%     end
+% 
+% 
+% end %--END OF FUNCTION
+% 
+% 
+% function Iout = falco_compute_subband_image_peak(mp, indexComboArray, iCombo, iSubband)
+% 
+%     % Generate the weighted, normalized intensity image for a single
+%     % wavelength, polarization, and star in the specified subband.
+% 
+%     % Extract indices
+%     iWavelength = indexComboArray(1, iCombo);
+%     iPol = indexComboArray(2, iCombo);
+%     iStar = indexComboArray(3, iCombo);
+% 
+%     % Compute E-field
+%     modvar = ModelVariables;
+%     modvar.whichSource = 'star';
+%     modvar.sbpIndex = iSubband;
+%     modvar.wpsbpIndex = iWavelength;
+%     modvar.starIndex = iStar;
+%     mp.full.polaxis = mp.full.pol_conds(iPol); % used only in PROPER full models
+%     Estar = model_full(mp, modvar);
+% 
+%     % Apply wavelength weight within subband.
+%     % Assume polarizations are evenly weighted.
+%     Iout = length(mp.full.pol_conds) * abs(Estar).^2;
+%     
+% end %--END OF FUNCTION
