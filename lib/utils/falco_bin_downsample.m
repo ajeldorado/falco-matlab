c6276119cd5cca68d73e9bb2dc88bc77da1118d7
A J Eldorado Riggs <aj.riggs@gmail.com>
2025-12-05 09:01:27 -0800

    Change downsampling to a much more efficient binning method.

--- lib/utils/falco_bin_downsample.m
+++ lib/utils/falco_bin_downsample.m
@@ -20,26 +20,27 @@ function Aout = falco_bin_downsample(Ain,dsfac)
 %     Aout : 2-D array
 %         Downsampled array
 
-    % Error checks on inputs
-    if(length(size(Ain))~=2); error('Ain must be a matrix.'); end
-    if(mod(dsfac,1)~=0); error('Argument dsfac must be an integer.'); end
-    
-    % Array Sizes
-    Nx0 = size(Ain,2);
-    Ny0 = size(Ain,1);
-    if(mod(Nx0,dsfac)~=0 || mod(Ny0,dsfac)~=0  )
-        error('The size of Ain must be divisible by dsfac.');
-    end
-    Nx1 = Nx0/dsfac;
-    Ny1 = Ny0/dsfac;
-    
-    % Bin and average values from the high-res array into the low-res array
-    Aout = zeros(Ny1,Nx1);
-    for ix = 1:Nx1
-        for iy = 1:Ny1
-            Aout(iy,ix) = sum(sum(Ain(dsfac*(iy-1)+1:dsfac*iy,dsfac*(ix-1)+1:dsfac*ix)))/dsfac^2;
-        end
-    end
+    Aout = falco_spatial_bin(Ain, dsfac)/(dsfac*dsfac);
+    % % Error checks on inputs
+    % if(length(size(Ain))~=2); error('Ain must be a matrix.'); end
+    % if(mod(dsfac,1)~=0); error('Argument dsfac must be an integer.'); end
+    % 
+    % % Array Sizes
+    % Nx0 = size(Ain,2);
+    % Ny0 = size(Ain,1);
+    % if(mod(Nx0,dsfac)~=0 || mod(Ny0,dsfac)~=0  )
+    %     error('The size of Ain must be divisible by dsfac.');
+    % end
+    % Nx1 = Nx0/dsfac;
+    % Ny1 = Ny0/dsfac;
+    % 
+    % % Bin and average values from the high-res array into the low-res array
+    % Aout = zeros(Ny1,Nx1);
+    % for ix = 1:Nx1
+    %     for iy = 1:Ny1
+    %         Aout(iy,ix) = sum(sum(Ain(dsfac*(iy-1)+1:dsfac*iy,dsfac*(ix-1)+1:dsfac*ix)))/dsfac^2;
+    %     end
+    % end
 
 end
 
