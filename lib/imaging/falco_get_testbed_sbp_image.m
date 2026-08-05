d607f7a23fb87adcb633c7f6b714f7e3acb1bc9c
gruane <gruane@jpl.nasa.gov>
2026-08-05 14:45:57 -0700

    Added functionality to get images from dst2r testbed

--- lib/imaging/falco_get_testbed_sbp_image.m
+++ lib/imaging/falco_get_testbed_sbp_image.m
@@ -33,6 +33,8 @@ function normI = falco_get_testbed_sbp_image(mp,si)
             normI = falco_get_dst_sbp_image(mp,si);
         case 'DST2'
             normI = falco_get_dst2_sbp_image(mp,si);
+        case 'DST2R'
+            normI = falco_get_dst2r_sbp_image(mp,si);
         case 'OMC'
             normI = falco_get_omc_sbp_image(mp,si);
         case 'IACT'
